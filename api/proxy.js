7ac4250ca85bece63114aa78e14a129c4ea16c5a
phuocnguyen2201 <phuocnguyen2201@gmail.com>
2025-08-05 14:40:37 +0300

    update CORS the required headers to allow cross-origin requests

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,11 @@
 export default async function handler(req, res) {
- const headers = {'content-type': 'application/json' ,'X-goog-api-key': `${process.env.GOOGLE_GEMINI_API_KEY}`};
-  const content = [{parts: [{text: 'Give 4 random colors pattern with hex code, just the hex code, make it an array'}]}];
 
+  const headers = {'content-type': 'application/json' ,'X-goog-api-key': `${process.env.GOOGLE_GEMINI_API_KEY}`};
+  const content = [{parts: [{text: 'Give 4 random colors pattern with hex code, just the hex code, make it an array'}]}];
+  
+  res.setHeader("Access-Control-Allow-Origin", "*"); // or restrict to specific origin
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   const response = await fetch(
     `${process.env.GOOGLE_GEMINI_API_URL}`,
     {
@@ -13,7 +17,7 @@ export default async function handler(req, res) {
     });
 
     const data = await response.json();
-    //Comment to checking
-
+    //Example response:
+    //{"colors":"```\n[\"#A8D5BA\", \"#F4B393\", \"#D6D2C4\", \"#3F72AF\"]\n```\n"}
     return res.status(200).json({ colors: data.candidates[0].content.parts[0].text });
 }
