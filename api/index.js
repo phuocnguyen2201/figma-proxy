bbb47a8de1f602663f649113c3ffcfe34be0e44f
phuocnguyen2201 <phuocnguyen2201@gmail.com>
2025-08-17 13:22:18 +0300

    fix the response object

--- api/index.js
+++ api/index.js
@@ -18,6 +18,6 @@ export const handler = async (event) => {
       const data = await response.json();
       //Example response:
       //{"colors":"```\n[\"#A8D5BA\", \"#F4B393\", \"#D6D2C4\", \"#3F72AF\"]\n```\n"}
-      return data.status == 200?{ colors: data.candidates[0].content.parts[0].text }:{ status: data.status};
+      return { colors: data.candidates[0].content.parts[0].text };
   };
   
